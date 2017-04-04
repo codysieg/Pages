8564b7adb20a150e0335c597f48c64268ac40ca8
Dhindsa91 <arvind.dhindsa1@gmail.com>
2017-04-03 22:46:03 -0700

    search function now works

--- searchBookDatabase.php
+++ searchBookDatabase.php
@@ -1,8 +1,6 @@
 <?php
-  session_start();
-  if(!isset($_SESSION['username'])){
-    header('Location: login.php');
-  }
+session_start();
+include 'databaseConnection.php';
 ?>
 
 <!DOCTYPE html>
@@ -10,121 +8,49 @@
 <head></head>
 <body>
   <?php
-  $check = 0;
+  
+  
   if ($_SERVER["REQUEST_METHOD"] == "POST"){
     //if any fields are set, appends them to SQL query
-    if($_POST["ISBN"] != "" || $_POST["title"] != "" || $_POST["authorFirst"] != "" || $_POST["authorLast"] != "" || $_POST["pcity"] != "" || $_POST["publisher"] != "" || $_POST["pdate"] != "" || $_POST["genre"] != ""){
-      $sql = "SELECT * FROM books WHERE";
-      if($_POST["ISBN"] != ""){
-        $ISBN = $_POST["ISBN"];
-        if($check==0){
-          $check=1;
-          $sql .= " ISBN LIKE \"%".$ISBN."%\"";
-        }
-        else{
-          $sql .= " and ISBN LIKE \"%".$ISBN."%\"";
-        }
+      $query = "SELECT * FROM attributes;";
+      $retval = mysqli_query($connection, $query);
+      
+      $check = 0;
+    while ($row = mysqli_fetch_array($retval)){
+      $attri = $row['attName'];
+      $search = $_POST[$row['attName']];
+    
+    if($_POST[$row['attName']] !="" &&  $check==0){
+     
+      $check = $check +1;
+      
+      $sql = "SELECT * FROM books WHERE $attri LIKE \"%".$search."%\"";
+ 
+      
       }
-      if($_POST["title"] != ""){
-        $title = $_POST["title"];
-        if($check==0){
-          $check=1;
-          $sql .= " title LIKE \"%".$title."%\"";
-        }
-        else{
-          $sql .= " and title LIKE \"%".$title."%\"";
-        }
-      }
-      if($_POST["authorFirst"] != ""){
-        $authorFirst = $_POST["authorFirst"];
-        if($check==0){
-          $check=1;
-          $sql .= " authorFirst LIKE \"%".$authorFirst."%\"";
-        }
-        else{
-          $sql .= " and authorFirst LIKE \"%".$authorFirst."%\"";
-        }
-      }
-      if($_POST["authorLast"] != ""){
-        $authorLast = $_POST["authorLast"];
-        if($check==0){
-          $check=1;
-          $sql .= " authorLast LIKE \"%".$authorLast."%\"";
-        }
-        else{
-          $sql .= " and authorLast LIKE \"%".$authorLast."%\"";
-        }
-      }
-      if($_POST["pcity"] != ""){
-        $pcity = $_POST["pcity"];
-        if($check==0){
-          $check=1;
-          $sql .= " pcity LIKE \"%".$pcity."%\"";
-        }
-        else{
-          $sql .= " and pcity LIKE \"%".$pcity."%\"";
-        }
-      }
-      if($_POST["publisher"] != ""){
-        $publisher = $_POST["publisher"];
-        if($check==0){
-          $check=1;
-          $sql .= " publisher LIKE \"%".$publisher."%\"";
-        }
-        else{
-          $sql .= " and publisher LIKE \"%".$publisher."%\"";
-        }
-      }
-      if($_POST["pdate"] != ""){
-        $pdate = $_POST["pdate"];
-        if($check==0){
-          $check=1;
-          $sql .= " pdate LIKE \"%".$pdate."%\"";
-        }
-        else{
-          $sql .= " and pdate LIKE \"%".$pdate."%\"";
-        }
-      }
-      if($_POST["genre"] != ""){
-        $genre = $_POST["genre"];
-        if($check==0){
-          $check=1;
-          $sql .= " genre LIKE \"%".$genre."%\"";
-        }
-        else{
-          $sql .= " and genre LIKE \"%".$genre."%\"";
-        }
-      }
-      include 'databaseConnection.php';
-      $_SESSION['sql'] = $sql;
-      header('Location: index.php');
+      else {
+       if($_POST[$row['attName']] !="" && $check>0){
+          $sql .= " OR $attri LIKE \"%".$search."%\";";
+        
+       }
     }
-    elseif($ISBN=="" && $title == "" && $authorFirst == "" && $authorLast == "" && $pcity == "" && $publisher == "" && $pdate == "" && $genre == ""){
-      include 'databaseConnection.php';
-      $sql = "SELECT * FROM books;";
-      $_SESSION['sql'] = $sql;
-      header('Location: index.php');
-      //fields not filled out properly, all are required. do nothing.
-      //possibly alert all fields need to be filled out.
-    }
-    else{
-      include 'databaseConnection.php';
-      //will return matches that start with entries.
-      $sql = "SELECT * FROM books;";
-      //pass sql
-      $_SESSION['sql'] = $sql;
-      //$retval = mysqli_query($connection, $sql);
-      //$_SESSION['retvalue'] = $retval;
-      //$row = mysqli_fetch_assoc($retval);
-      //$_SESSION['row'] = $row;
-
-      header('Location: index.php');
-    }
-  }
-
+        
+       
+        }
+        
+    
+     $_SESSION['sql'] = $sql;
+    echo  $_SESSION['sql'] ;
+    //  header("Location:index.php");
+      }
+  
+   
+     
+    
+    
   else{
     header('Location: index.php');
   }
-?>
+?>  
 </body>
 </html>
